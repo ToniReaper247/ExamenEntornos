5b6aa56655c09aab8ace13595a2132e39d3fcfdd
ToniReaper247 <75076914+ToniReaper247@users.noreply.github.com>
2020-12-10 13:32:18 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-
+//Esto es un comentario con una falta otrografica
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("HOLA MUNDO");
+		System.out.println("Antonio");
 	}
 
 }
